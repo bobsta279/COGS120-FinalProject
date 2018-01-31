571c2bbe61919657cfa9badee9972376ec656d33
Chen Yang <cyang@godaddy.com>
2018-01-30 23:37:36 -0800

    fix repo for slides

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +37,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
 

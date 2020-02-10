23b85b5070725a32f81af2bbf3087dc0fca438cb
emilyferguson989 <emilyferguson989@gmail.com>
2020-02-09 22:48:52 -0800

    Got the filters done

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var posts = require('./routes/posts');
+var maingym = require('./routes/maingym');
+var rimac = require('./routes/rimac');
+var expert = require('./routes/expert');
+var intermediate = require('./routes/intermediate');
+var novice = require('./routes/novice');
+var maingym_novice = require('./routes/maingym-novice');
+var maingym_intermediate = require('./routes/maingym-intermediate');
+var maingym_expert = require('./routes/maingym-expert');
+var rimac_novice = require('./routes/rimac-novice');
+var rimac_intermediate = require('./routes/rimac-intermediate');
+var rimac_expert = require('./routes/rimac-expert');
+
+
 var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
@@ -39,6 +52,17 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/posts', posts.viewPost);
+app.get('/maingym', maingym.view);
+app.get('/rimac', rimac.view);
+app.get('/novice', novice.view);
+app.get('/intermediate', intermediate.view);
+app.get('/expert', expert.view);
+app.get('/maingym-novice', maingym_novice.view);
+app.get('/maingym-intermediate', maingym_intermediate.view);
+app.get('/maingym-expert', maingym_expert.view);
+app.get('/rimac-novice', rimac_novice.view);
+app.get('/rimac-intermediate', rimac_intermediate.view);
+app.get('/rimac-expert', rimac_expert.view);
 app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);

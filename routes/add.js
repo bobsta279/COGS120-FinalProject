3c7029525dc0994c802cd47f1d57ae0b0d4bbc9c
AlexL <agarza@ucsd.edu>
2020-02-15 14:42:22 -0800

    Alwoob

--- routes/add.js
+++ routes/add.js
@@ -4,4 +4,4 @@ exports.addPost = function(req, res){
     var myJson = {"name": req.query.name, "goals": req.query.description};
     data.posts.push(myJson);
     res.render('index', data);
-  };
+};

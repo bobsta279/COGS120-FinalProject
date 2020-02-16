7a93860605535c45a56cbe62569249cd39111054
AlexL <agarza@ucsd.edu>
2020-02-15 21:03:39 -0800

    Name stays

--- routes/savePost.js
+++ routes/savePost.js
@@ -4,23 +4,7 @@ var filteredData;
 
 exports.addPost = function(req, res){  
   user.wantsTo = req.query.wantsTo;
-  var myJson = {"name": user.name, "level": user.userLevel, "gym": user.gym, "phone": user.phone, "wantsToLearn": user.wantsTo, "when": user.when};
+  var myJson = {"name": user.firstName + user.lastName, "level": user.userLevel, "gym": user.gym, "phone": user.phone, "wantsToLearn": user.wantsTo, "when": user.when};
   data.posts.unshift(myJson);
-
-  filteredData = JSON.parse(JSON.stringify(data));
-
-  console.log(user);
-  var expString = "";
-  var gymString = "";
-  for (i = 0; i < filteredData.posts.length; i++){
-    expString = filteredData.posts[i].level.toLowerCase();
-    gymString = filteredData.posts[i].gym;
-    if (expString.localeCompare(user.level) || gymString.localeCompare(user.gym)){
-        filteredData.posts.splice(i, 1);
-        i--;
-    }
-  }
-
-  res.render('recommended', filteredData);
-  filteredData = JSON.parse(JSON.stringify(data));
+  res.render('index', data);
 };
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,12 @@ var data = require("../user.json");
 var posts = require("../posts.json");
 
 exports.addInfo = function(req, res){
-    data.name = (req.query.firstName + " " + req.query.lastName);
+    if(("" + req.query.firstName).localeCompare("")){
+        data.firstName = req.query.firstName + " ";
+    }
+    if(("" + req.query.lastName).localeCompare("")){
+        data.lastName = req.query.lastName;
+    }
     data.userLevel = (req.query.dropdown);
 
     res.render('index', posts);

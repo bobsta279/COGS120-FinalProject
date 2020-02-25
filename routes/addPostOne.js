8074a65f23aa9023924c1d41a44223c8bd374a6c
AlexL <agarza@ucsd.edu>
2020-02-24 18:20:04 -0800

    Create fix

--- routes/addPostOne.js
+++ routes/addPostOne.js
@@ -1,8 +1,13 @@
 var data = require("../user.json");
+var isEntered = true;
 
 exports.addInfo = function(req, res){
-    data.when = req.query.dropdown;
-
-    console.log("Add1");
-    res.render('add-post2');
+    if (req.query.dropdown == "none"){
+        isEntered = false;
+        res.render('add-post')
+    } else {
+       data.when = req.query.dropdown;
+       console.log("Add1");
+       res.render('add-post2');
+    }
 };

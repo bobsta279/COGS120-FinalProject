8a58257709bf62a0f74641d824dc317923c8658c
AlexL <agarza@ucsd.edu>
2020-02-24 22:38:45 -0800

    Fixed create profile and login

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,26 @@ var posts = require("../posts.json");
 exports.addInfo = function(req, res){
     if(("" + req.query.firstName).localeCompare("")){
         data.firstName = req.query.firstName + " ";
+        firstName = data.firstName;
+    } else {
+        req.query.firstName = data.firstName;
     }
+
     if(("" + req.query.lastName).localeCompare("")){
         data.lastName = req.query.lastName;
+    } else {
+        req.query.lastName = data.lastName;
     }
-    data.userLevel = (req.query.dropdown);
 
-    res.render('index', posts);
-};
+    if(req.query.dropdown != "none"){
+        data.userLevel = (req.query.dropdown);
+    }
+
+    if(("" + req.query.lastName).localeCompare("") && 
+        ("" + req.query.firstName).localeCompare("") &&
+        req.query.dropdown != "none"){
+            data.userLevel = (req.query.dropdown);
+
+            res.render('index', posts);
+    } 
+}

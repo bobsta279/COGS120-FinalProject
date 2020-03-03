afc3304d1fa935e95529f307e33507b1208a6dc3
AlexL <agarza@ucsd.edu>
2020-03-03 12:43:48 -0800

    more json ppl

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,18 @@
+var user = require("../user.json");
 exports.view = function(req, res){
+  user.firstName = "";
+  user.lastName = "";
+  user.gym = "";
+  user.level = "";
+  user.when = "";
+  user.phone = "";
+  user.userLevel = "";
+  user.wantsTo = "";
+  user.longTerm = "";
+  user.noPrefWhen = "true";
+  user.noPrefLoc = "true";
+  user.noPrefFoc = "true";
+  user.from = "true";
+
   res.render('login');
 };
